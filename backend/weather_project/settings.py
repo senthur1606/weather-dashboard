45da890932b22f70e7723f0efc75bb2c18f2ae3b
Senthur pandian <skpandian8@gmail.com>
2026-06-23 08:15:37 +0530

    allow cross origns

--- backend/weather_project/settings.py
+++ backend/weather_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ===== SECURITY =====
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-skypulse-dev-key-change-in-production-abc123xyz')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0,weather-dashboard-dywt.vercel.app/').split(',')
 GEMINI_API_KEY=  config("GEMINI_API_KEY", default="")
 # ===== APPS =====
 INSTALLED_APPS = [
@@ -152,7 +152,7 @@ SIMPLE_JWT = {
 # ===== CORS =====
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000'
+    default='http://localhost:3000,http://127.0.0.1:3000,https://weather-dashboard-dywt.vercel.app,'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['*']
